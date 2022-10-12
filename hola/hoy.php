346b47a6c7207d59c26061d01b1879d1f48f3b4e
bmjulian <julian20426@cbtis75.edu.mx>
2022-10-12 09:09:25 -0500

    Create hoy.php

---
+++ hola/hoy.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hoy</title>
+</head>
+<body>
+    <body background¿
+    <center>
+        <font color="white">
+        <style type="text/css">
+.verde{
+    color: green;
+}
+</style>
+    <?php
+    echo "<>";
+    echo "<h1>";
+    echo date('d-m-y');
+    echo "</h1>";
+    ?>
+    </font>
+    </center>
+</body>
+</html>
