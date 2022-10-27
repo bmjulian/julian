18b4551de7e4614d97ce2d3a8106f09923892f27
bmjulian <julian20426@cbtis75.edu.mx>
2022-10-26 23:19:42 -0500

    Create dias2.php

---
+++ ACTIVIDAD 7/DIAS VIVIDOS/PHP/dias2/dias2.php
@@ -0,0 +1,31 @@
+<!-- 
+    JULIAN JESUS BUENROSTRO Martinez
+    26/10/22
+ -->
+ <!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dias vividos</title>
+</head>
+<body>
+    <body bgcolor="gray">
+    <form action="dias1.php" method="get">
+        <center>
+            <div class="titulo">
+                <h1>Dias vividos</h1>
+            </div>
+            <hr>
+            <div class="Formulario">
+                    <input type="number" name="edad" id="edad" placeholder="Ingresa la edad deseada "><br><br>
+                    <button type="submit"> CALCULAR</button>
+                
+            </div>
+        </center>
+    </form>
+    </div>
+</body>
+</html>
