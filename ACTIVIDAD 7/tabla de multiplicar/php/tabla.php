1674826432f9c140282e34bcf865e66efbab84a0
bmjulian <julian20426@cbtis75.edu.mx>
2022-10-26 23:19:53 -0500

    Create tabla.php

---
+++ ACTIVIDAD 7/tabla de multiplicar/php/tabla.php
@@ -0,0 +1,18 @@
+<!-- 
+    JULIAN JESUS BUENROSTRO MARTINEZ
+    26/10/22
+ -->
+ <?php
+    if (isset($_GET["renglon"]) && isset($_GET["columna"]))
+    echo "<table border='1'>";
+    for($ren=1; $ren <=$_GET['renglon']; $ren++){
+        echo "<tr>";
+        for($col=1; $col <= $_GET['columna']; $col++){
+            echo "<td>";
+            echo "$ren$col";
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+?>
